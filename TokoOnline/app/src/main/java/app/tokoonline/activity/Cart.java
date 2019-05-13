1d8085da2896fd097208732dabe270b2d155abef
bayuadam13 <d.bayuadam13@gmail.com>
2019-05-14 01:02:44 +0700

    Project kita
    Sudah Ada Log in dan registernya gan

---
+++ TokoOnline/app/src/main/java/app/tokoonline/activity/Cart.java
@@ -0,0 +1,65 @@
+package app.tokoonline.activity;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import app.tokoonline.R;
+import app.tokoonline.adapter.CartAdapter;
+import app.tokoonline.model.CartModel;
+
+public class Cart extends AppCompatActivity {
+
+    SwipeRefreshLayout swipe;
+    RecyclerView recyclerView;
+    ArrayList<CartModel> carts;
+    CartAdapter cartAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        swipe           = findViewById(R.id.swipe);
+        recyclerView    = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        carts           = new ArrayList<>();
+        setCart();
+
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                carts.clear(); recyclerView.setAdapter(null);
+                setCart();
+            }
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Keranjang");
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+
+    private void setCart(){
+        for (int i=0; i < 3; i++) {
+            CartModel model = new CartModel();
+            carts.add(model);
+        }
+
+        cartAdapter = new CartAdapter(Cart.this, carts);
+        cartAdapter.notifyDataSetChanged();
+        Log.d("_COUNT", String.valueOf(cartAdapter.getItemCount()) );
+        recyclerView.setAdapter(cartAdapter);
+
+        swipe.setRefreshing(false);
+    }
+}
---
+++ TokoOnline/app/src/main/java/app/tokoonline/model/CartModel.java
@@ -0,0 +1,10 @@
+package app.tokoonline.model;
+
+/**
+ * Created by Lazday Indonesia
+ * https://lazday.com
+ * On 11/13/2017.
+ */
+
+public class CartModel {
+}
